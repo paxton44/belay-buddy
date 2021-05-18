2ab342b861e06fb325c9ad296ca9901e7865c41c
Mitchell Flood <mitchflood1@gmail.com>
2021-05-17 22:02:52 -0600

    fixing routing

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/readingdb",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 // Start the API server
 app.listen(PORT, function() {
