958254077b2d309f8dea5fd2e59e812dc50307d0
wylie-u <wylieurig@gmail.com>
2021-06-05 13:50:10 -0600

    deploying with heroku

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,9 +3,16 @@ const db = require("../models");
 
 // Defining methods for the userProfilesController
 module.exports = {
+  findAll: function(req, res) {
+    console.log(res)
+    db.UserProfile 
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   
   findById: function (req, res) {
-    db.userProfile.findById(req.params.id)
+    db.UserProfile.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
