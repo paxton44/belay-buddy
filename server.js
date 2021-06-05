46fab8c4090bac5d4de0b8a84aa2f6f8c38cc29b
wylie-u <wylieurig@gmail.com>
2021-06-05 14:52:31 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../build")));
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
