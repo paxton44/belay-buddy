44452da2ed7f7759667b74bc7f67130bf5a34dc7
Mitchell Flood <mitchflood1@gmail.com>
2021-06-09 14:47:48 -0600

    fixing server for heroku deploy

--- server.js
+++ server.js
@@ -40,12 +40,14 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  
 }
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // Express Session
+})
 
 
 // Connect to the Mongo DB
